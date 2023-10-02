0428f43b15b4461e43bc763f0ddbdeb9ec2fb8ac
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2023-10-02 08:57:28 +0530

    navbar completed
    not reponsive

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,24 +1,80 @@
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { useRouter } from 'next/router'
+import { DribbbleIcon, GithubIcon, LinkedInIcon, PinterestIcon, TwitterIcon } from './Icons';
+
+import { motion } from 'framer-motion'
+
+// custom styles to link tags
+const CustomLink = ({ href, title, className = '' }) => {
+  const router = useRouter();
+
+  return (
+    <Link href={href} className={`${className} relative group`}>
+      {title}
+      <span
+        className={`h-[2px] inline-block w-0 bg-dark absolute left-0 -bottom-0.5 group-hover:w-full transition-[width] ease duration-300 ${
+          router.asPath === href ? 'w-full' : 'w-0'
+        }`}
+      >
+        &nbsp;
+      </span>
+    </Link>
+  );
+};
 
 const NavBar = () => {
   return (
     <header className='w-full px-32 py-8 font-medium flex items-center justify-between'>
         <nav>
-            <Link href="/">Home</Link>
-            <Link href="/about">About-us</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/articles">Articles</Link>
+            <CustomLink href="/" title="Home" className='mr-4'/>
+            <CustomLink href="/about" title="About" className='mx-4' />
+            <CustomLink href="/projects" title="Projects" className='mx-4' />
+            <CustomLink href="/articles" title="Articles" className='ml-4' />
         </nav>
 
-        <nav>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
-            <Link href="/" target={'_blank'}>T</Link>
+        <nav className='flex items-center justify-center flex-wrap'>
+            <motion.a 
+              href="https://twitter.com" 
+              target={'_blank'}
+              whileHover={{ y:-2 }}
+              whileTap={{ scale: 0.9 }}
+              className='w-6 mr-3'
+            >
+              <TwitterIcon />
+            </motion.a>
+
+            <motion.a 
+              href="https://github.com" 
+              target={'_blank'}
+              whileHover={{ y:-2 }}
+              whileTap={{ scale: 0.9 }}
+              className='w-6 mx-3'
+            >
+              <GithubIcon />
+            </motion.a>
+
+            <motion.a 
+              href="https://linkedin.com" 
+              target={'_blank'}
+              whileHover={{ y:-2 }}
+              whileTap={{ scale: 0.9 }}
+              className='w-6 mx-3'
+            >
+              <LinkedInIcon />
+            </motion.a>
+
+            <motion.a 
+              href="https://pintrest.com" 
+              target={'_blank'}
+              whileHover={{ y:-2 }}
+              whileTap={{ scale: 0.9 }}
+              className='w-6 ml-3'
+            >
+              <PinterestIcon />
+            </motion.a>
+
         </nav>
 
         <div className='absolute left-[50%] top-2 translate-x-[-50%]'>
