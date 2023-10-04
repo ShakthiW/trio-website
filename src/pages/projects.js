9f55d3f41a6f81a0088d50880ad4bfb0e5f00768
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2023-10-04 18:15:32 +0530

    Update projects.js

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,7 +1,92 @@
 import AnimatedText from "@/components/AnimatedText";
+import { GithubIcon } from "@/components/Icons";
 import Layout from "@/components/Layout";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import Project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
+
+const FeaturedProject = ({ type, title, summary, img, link, github }) => {
+  return (
+    <article className="w-full flex items-center justify-between rounded-3xl border border-solid border-dark bg-light shadow-2xl p-12 relative">
+      <Link
+        href={link}
+        target={"_blank"}
+        className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-1/2 flex flex-col items-start justify-between pl-6">
+        <span className="text-primary font-medium text-xl">{type}</span>
+
+        <Link
+          href={link}
+          target={"_blank"}
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-4xl font-bold">{title}</h2>
+        </Link>
+
+        <p className="my-2 font-medium text-dark">{summary}</p>
+
+        <div className="mt-2 flex items-center">
+          <Link href={github} target={"_blank"} className="w-10">
+            <GithubIcon />
+          </Link>
+          <Link
+            href={link}
+            target={"_blank"}
+            className="ml-4 rounded-lg bg-dark text-light p-2 px-6 text-lg font-semibold"
+          >
+            Visit Project
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+
+const Project = ({ title, type, img, link, github }) => {
+  return (
+    <article className="w-full flex flex-col items-center justify-center rounded-2xl border border-solid border-dark bg-light p-6 relative">
+      <Link
+        href={link}
+        target={"_blank"}
+        className="w-full cursor-pointer overflow-hidden rounded-lg"
+      >
+        <Image src={img} alt={title} className="w-full h-auto" />
+      </Link>
+
+      <div className="w-full flex flex-col items-start justify-between mt-4">
+        <span className="text-primary font-medium text-xl">{type}</span>
+
+        <Link
+          href={link}
+          target={"_blank"}
+          className="hover:underline underline-offset-2"
+        >
+          <h2 className="my-2 w-full text-left text-3xl font-bold">{title}</h2>
+        </Link>
+
+        <div className="mt-2 flex items-center justify-between w-full">
+          <Link
+            href={link}
+            target={"_blank"}
+            className=" text-lg font-semibold underline"
+          >
+            Visit
+          </Link>
+          <Link href={github} target={"_blank"} className="w-8">
+            <GithubIcon />
+          </Link>
+        </div>
+      </div>
+    </article>
+  );
+};
 
 const projects = () => {
   return (
@@ -16,7 +101,74 @@ const projects = () => {
 
       <main className="w-full">
         <Layout className="pt-16">
-            <AnimatedText text="Imagination Trumps Knowledge!" />
+          <AnimatedText
+            text="Imagination Trumps Knowledge!"
+            className="mb-16"
+          />
+
+          <div className="grid grid-cols-12 gap-24">
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Crypto Screener Application"
+                type="FeaturedProject"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                img={Project1}
+                link="https://crypto-screener.netlify.app/"
+                github="https://github.com/ShakhthiW/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                type="FeaturedProject"
+                img={Project1}
+                link="https://crypto-screener.netlify.app/"
+                github="https://github.com/ShakhthiW/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                type="FeaturedProject"
+                img={Project1}
+                link="https://crypto-screener.netlify.app/"
+                github="https://github.com/ShakhthiW/"
+              />
+            </div>
+
+            <div className="col-span-12">
+              <FeaturedProject
+                title="Crypto Screener Application"
+                type="FeaturedProject"
+                summary="A feature-rich Crypto Screener App using React, Tailwind CSS, Context API, React Router and Recharts. 
+It shows detail regarding almost all the cryptocurrency. You can easily convert the price in your 
+local currency."
+                img={Project1}
+                link="https://crypto-screener.netlify.app/"
+                github="https://github.com/ShakhthiW/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                type="FeaturedProject"
+                img={Project1}
+                link="https://crypto-screener.netlify.app/"
+                github="https://github.com/ShakhthiW/"
+              />
+            </div>
+            <div className="col-span-6">
+              <Project
+                title="Crypto Screener Application"
+                type="FeaturedProject"
+                img={Project1}
+                link="https://crypto-screener.netlify.app/"
+                github="https://github.com/ShakhthiW/"
+              />
+            </div>
+          </div>
         </Layout>
       </main>
     </>
