8c68f6e26832fabf1c572d416d44e24aa17b7394
Shakthi Warnakulasuriya <shakthi.20221274@iit.ac.lk>
2023-10-06 09:51:15 +0530

    made home page responsive

--- src/components/AnimatedText.js
+++ src/components/AnimatedText.js
@@ -32,7 +32,7 @@ const singleWord = {
 
 const AnimatedText = ({ text, className="" }) => {
   return (
-    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden'>
+    <div className='w-full mx-auto py-2 flex items-center justify-center text-center overflow-hidden sm:py-0'>
         <motion.h1 
             className={`inline-block w-full text-dark font-bold capitalize text-8xl dark:text-light ${className}`}
             variants={quote}
--- src/components/HireUs.js
+++ src/components/HireUs.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { CircularText } from './Icons';
-import Link from 'next/link';
+import React from "react";
+import { CircularText } from "./Icons";
+import Link from "next/link";
 
 const HireUs = () => {
   return (
-    <div className='fixed left-4 bottom-4 flex items-center justify-center overflow-hidden'>
-      <div className='w-48 h-auto flex items-center justify-center relative'>
-        <CircularText className={'fill-dark animate-spin-slow dark:fill-light'} />
+    <div className="fixed left-4 bottom-4 flex items-center justify-center overflow-hidden md:right-8 md:left-auto md:top-0 md:bottom-auto md:absolute">
+      <div className="w-48 h-auto flex items-center justify-center relative md:w-24">
+        <CircularText
+          className={"fill-dark animate-spin-slow dark:fill-light"}
+        />
 
-        <Link href="mailto:contact@trio.software" className='flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light'>
-            Hire Us
+        <Link
+          href="mailto:contact@trio.software"
+          className="flex items-center justify-center absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 bg-dark text-light shadow-md border border-solid border-dark w-20 h-20 rounded-full font-semibold hover:bg-light hover:text-dark dark:bg-light dark:text-dark hover:dark:bg-dark hover:dark:text-light hover:dark:border-light md:w-12 md:h-12 md:text-[10px]"
+        >
+          Hire Us
         </Link>
       </div>
     </div>
